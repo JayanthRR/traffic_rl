fixed lookahead computation, and boltzmann underflow issue

main.py
@@ -9,7 +9,7 @@ from exp_plots import plot
 
 marker_1 = itertools.cycle(('X', '+', 'o', '.', '*', '-', '1', '2', '3', '4', '5'))
 
-config = {"num episodes": 5000,
+config = {"num episodes": 6000,
           "epsilon": 0.2,
           "gamma": 1,
           "sparsity": 0.1,
@@ -28,7 +28,9 @@ config = {"num episodes": 5000,
                    0: {"algorithm": "qlearning"},
                    1: {"algorithm": "const_dijkstra", "lookahead": 0},
                    2: {"algorithm": "const_dijkstra", "lookahead": 1},
-                   3: {"algorithm": "expected_dijkstra"},
+                   3: {"algorithm": "const_dijkstra", "lookahead": 5},
+                   4: {"algorithm": "const_dijkstra", "lookahead": 10},
+                   5: {"algorithm": "expected_dijkstra"},
                    }
           }
 
@@ -96,22 +98,24 @@ def genAfortrain(config):
 
 def evaluate(args):
 
-    env, agent, W, config, config_id = args
+    env, agent, W, config, config_id, variance = args
     algo = config[config_id]["algorithm"]
 
     if algo in ["qlearning", "sarsa"]:
         agent[algo].env = env
-        reward, _, path = evaluate_rl_policy(agent[algo], W)
+        reward, _, path, states = evaluate_rl_policy(agent[algo], W)
     elif algo in ["const_dijkstra"]:
         lookahead = config[config_id]["lookahead"]
-        reward, _, path = evaluate_policies(env, W, algo, lookahead, const_flag=True)
+        reward, _, path, states = evaluate_policies(env, W, algo, lookahead, const_flag=True, sigma=variance)
     else:
         # expected dijkstra
-        reward, _, path = evaluate_policies(env, W, algo, lookahead=0, const_flag=True, expected_flag=True)
+        reward, _, path, states = evaluate_policies(env, W, algo, lookahead=0, const_flag=True,
+                                            expected_flag=True, sigma=variance)
 
     logs = dict()
     logs["rewards"] = reward
     logs["path"] = path
+    logs["states"] = states
     gc.collect()
     del env, agent, W
     return logs
@@ -133,6 +137,7 @@ def test(config, folder):
         logdict[ind] = dict()
         logdict[ind]["rewards"] = []
         logdict[ind]["path"] = []
+        logdict[ind]["states"] = []
 
     with open(folder+"env.p", "rb") as f:
         env = pickle.load(f)
@@ -155,12 +160,25 @@ def test(config, folder):
         env.reset()
         logs={}
 
+        min_state_len = np.float("inf")
         for k in test_config.keys():
-            logs[k] = evaluate([copy.deepcopy(env), agent, W, test_config, k])
+            logs[k] = evaluate([copy.deepcopy(env), agent, W, test_config, k, config["noise variance"]])
+            if len(logs[k]["states"]) < min_state_len:
+                min_state_len = len(logs[k]["states"])
+
+        for stateind in range(min_state_len):
+            for ind in test_config.keys():
+                try:
+                    assert((logs[ind]["states"][stateind] == logs[0]["states"][stateind]).all())
+                except:
+                    print("Something fishy", ind)
 
         for ind in test_config.keys():
             logdict[ind]["rewards"].append(logs[ind]["rewards"])
             logdict[ind]["path"].append(logs[ind]["path"])
+            logdict[ind]["states"].append(logs[ind]["states"])
+
+
         gc.collect()
 
     log_file = folder + "logs.p"
@@ -183,8 +201,8 @@ def run(config, A, source, destination, costdict, folder):
     noise_amp = config["noise amplitude"]
     be = config["basis expansion"]
 
-    # x_init = np.random.rand(size)
-    x_init = np.random.uniform(low=0.01, high=0.1, size=(size,))
+    x_init = np.random.rand(size)
+    # x_init = np.random.uniform(low=0.01, high=0.1, size=(size,))
     # x_init = x_init / x_init.sum()
 
     env = TrafficEnv(A, x_init, source, destination, costdict, be,
@@ -200,6 +218,14 @@ def run(config, A, source, destination, costdict, folder):
 
     test(config, folder)
     print("completed testing ...")
+
+
+def test_from_logs(folder_name):
+
+    with open(folder_name +"config.p", "rb") as f:
+        config = pickle.load(f)
+
+    test(config, folder_name)
 
 
 def gencostfn(cfn, size):
@@ -221,27 +247,33 @@ if __name__ == "__main__":
 
     config["sparsity"] = 0.05
 
-    root_folder = "logs/" + datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S") + "/"
+    test_directly = False
 
-    for be in [0]:
-        config["basis expansion"] = be
+    if test_directly:
+        test_from_logs("logs/2019-03-16-08-25-24/exp_4/100/0.02_2019-03-16-08-25-24/")
+    else:
 
-        for cfn in [1]:
-            config["costfn"] = cfn
-            folder = root_folder + "exp_" + str(3*be+cfn) + "/"
+        root_folder = "logs/" + datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S") + "/"
 
-            for siz in [100]:
-                config["size"] = siz
-                costdict = gencostfn(cfn, siz)
+        for be in [1]:
+            config["basis expansion"] = be
 
-                np.random.seed(2134)
+            for cfn in [1]:
+                config["costfn"] = cfn
+                folder = root_folder + "exp_" + str(3*be+cfn) + "/"
 
-                A, source, destination, _ = genAfortrain(config)
+                for siz in [100]:
+                    config["size"] = siz
+                    costdict = gencostfn(cfn, siz)
 
-                for var in tqdm([0.02, 0.04, 0.06, 0.08, 0.1]):
-                    config["noise variance"] = var
+                    np.random.seed(1234)
 
-                    run(config, A, source, destination, costdict,
-                        folder + str(siz) + "/" + str(var) + "_")
+                    A, source, destination, _ = genAfortrain(config)
 
-    plot(root_folder)
+                    for var in tqdm([0.02, 0.04, 0.06, 0.08, 0.1, 0.15]):
+                        config["noise variance"] = var
+
+                        run(config, A, source, destination, costdict,
+                            folder + str(siz) + "/" + str(var) + "_")
+
+        plot(root_folder)

rl.py
@@ -1,6 +1,6 @@
 import numpy as np
 from random import choice
-# from matplotlib import pyplot as plt
+from matplotlib import pyplot as plt
 import pickle
 from graphs import *
 import copy
@@ -149,8 +149,8 @@ class TrafficEnv:
 
     def random_init(self):
         # np.random.seed()
-        # self.xt = np.random.rand(self.size)
-        self.xt = np.random.uniform(low=0.01, high=0.1, size=(self.size, ))
+        self.xt = np.random.rand(self.size)
+        # self.xt = np.random.uniform(low=0.01, high=0.1, size=(self.size, ))
         # self.xt = self.xt/self.xt.sum()
 
     def get_successor_edges(self, current_edge=None):
@@ -158,10 +158,13 @@ class TrafficEnv:
         if not current_edge:
             current_edge = self.current_edge
         for i in range(self.transition_matrix.shape[0]):
-            if self.transition_matrix[i, current_edge] > 0:
-                if i == current_edge:
-                    continue
-                adj.append(i)
+            try:
+                if self.transition_matrix[i, current_edge] > 0:
+                    if i == current_edge:
+                        continue
+                    adj.append(i)
+            except ValueError:
+                print("error")
         return adj
 
     def get_predecessor_edges(self, current_edge=None):
@@ -227,7 +230,9 @@ class TrafficAgent:
             return None
 
         values = [self.q_function.evaluate(self.env.state, action) for action in candidate_actions]
-        probs = [np.exp(value/temp) for value in values]
+        maxval = max(values)
+        probs = [np.exp((value-maxval)/temp) for value in values]
+        # probs = [np.exp((value)/temp) for value in values]
 
         indices = np.argsort(probs)
         if sum(probs)==0:
@@ -326,7 +331,7 @@ def train_agent(agent, num_episodes, discount_factor,
     training_rewards = []
     total_rewards = []
     init_learning_rate = agent.learning_rate
-    temperature = 100
+    temperature = 50
     decayflag = False
     cntdecayflag = 0
 
@@ -354,7 +359,7 @@ def train_agent(agent, num_episodes, discount_factor,
             if action == agent.env.destination:
                 target_diff = step_reward - agent.q_function.evaluate(state, action)
                 cntdecayflag += 1
-                if cntdecayflag == 100:
+                if cntdecayflag == 1:
                     decayflag = True
 
                 if target_diff > MAX_W:
@@ -433,13 +438,13 @@ def train_agent(agent, num_episodes, discount_factor,
             print("episode: ", episode, len(episode_path))
             # agent.epsilon = agent.epsilon * agent.exploration_decay
         if decayflag:
-            temperature = max(temperature * 0.99, 0.01)
+            temperature = max(temperature * 0.99, 0.1)
             agent.epsilon = agent.epsilon * agent.exploration_decay
 
     return agent, training_rewards, total_rewards
 
 
-def evaluate_policies(env, W, policy="dijkstra", lookahead=0, const_flag=False, expected_flag=False):
+def evaluate_policies(env, W, policy="dijkstra", lookahead=0, const_flag=False, expected_flag=False, sigma=0.01):
 
     if policy == "dijkstra":
         control = dijkstra_policy
@@ -453,13 +458,15 @@ def evaluate_policies(env, W, policy="dijkstra", lookahead=0, const_flag=False, 
     reward_incurred = 0
     aggr_reward.append(reward_incurred)
     count = 0
+    states = []
+
     while env.current_edge is not env.destination:
 
         # for i in range(lookahead):
         #     env.state_transition()
         xt = env.xt
         for look in range(lookahead):
-            xt = state_transition(env.transition_matrix, xt, env.current_edge, env.destination)
+            xt = state_transition(env.transition_matrix, xt, wt=None, sigma=sigma)
 
         if const_flag:
             if not const_path:
@@ -467,7 +474,7 @@ def evaluate_policies(env, W, policy="dijkstra", lookahead=0, const_flag=False, 
                 if expected_flag:
                     ext = np.zeros_like(xt)
                     for expi in range(10):
-                        xt = state_transition(env.transition_matrix, xt, env.current_edge, env.destination)
+                        xt = state_transition(env.transition_matrix, xt, wt=None, sigma=sigma)
                         ext += xt
                     xt = ext/10
 
@@ -499,14 +506,15 @@ def evaluate_policies(env, W, policy="dijkstra", lookahead=0, const_flag=False, 
 
         env.state_transition(noise_t=wt)
         next_state, reward = env.get_next_state_reward(decision)
+        states.append(env.state)
 
         reward_incurred += reward
-        aggr_reward.append(reward_incurred)
+        aggr_reward.append(reward)
         count += 1
         if count == len(env.xt):
             break
 
-    return reward_incurred, aggr_reward, path_taken
+    return reward_incurred, aggr_reward, path_taken, states
 
 
 def evaluate_rl_policy(traffic_agent, W):
@@ -517,11 +525,14 @@ def evaluate_rl_policy(traffic_agent, W):
     reward_incurred = 0
     aggr_reward.append(reward_incurred)
     count = 0
+    qsa = []
+    states = []
 
     while traffic_agent.env.current_edge is not traffic_agent.env.destination:
         # Todo: signal flag
 
         decision = traffic_agent.get_action()
+        qsa.append(traffic_agent.q_function.evaluate(traffic_agent.env.state, decision))
 
         path_taken.append(decision)
 
@@ -534,13 +545,18 @@ def evaluate_rl_policy(traffic_agent, W):
 
         traffic_agent.env.state_transition(noise_t=wt)
         next_state, reward = traffic_agent.env.get_next_state_reward(decision)
+        states.append(traffic_agent.env.state)
 
         reward_incurred += reward
-        aggr_reward.append(reward_incurred)
+        aggr_reward.append(reward)
         count += 1
         if count == 5*len(wt):
             print("exceeded time limit")
             break
 
-    return reward_incurred, aggr_reward, path_taken
+    plt.plot(qsa)
+    plt.axhline(y=reward_incurred, color='r', linestyle='-')
+    plt.show()
+
+    return reward_incurred, aggr_reward, path_taken, states
 